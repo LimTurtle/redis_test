7d8ce06a5a885dd2ead27b6516beab222461fb1e
LimTurtle <pigwoo98@gmail.com>
2023-02-01 01:26:07 +0900

    C++ Redis 연동 및 클래스화

---
+++ cpp_redis/cpp_redis/cpp_redis.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Redis.h"
+
+using namespace std;
+
+int main()
+{
+    Redis* redis = new Redis(); //redis 초기설정
+    
+    redis->RedisQuery("SET foo hello"); //또는 redis->RedisQuery("SET %s %s", "foo", "hello")
+    const char* res = redis->RedisQuery("GET foo"); //또는 redis->RedisQuery("GET %s", "foo")
+    cout << res << endl;
+    return 0;
+}
