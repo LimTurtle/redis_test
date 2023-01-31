7d3e9f71d98bca4691d79e75b464412053f8ac27
LimTurtle <pigwoo98@gmail.com>
2023-01-31 18:26:26 +0900

    Redis 서버 외부 연결 허용

--- redis_mysql_compare/redis_connect.py
+++ redis_mysql_compare/redis_connect.py
@@ -1,5 +1,5 @@
 import redis
-r = redis.Redis(host="127.0.0.1", port="6379", charset="utf-8", decode_responses=True)
+r = redis.Redis(host="34.84.148.50", port="6379", password="a12b34", charset="utf-8", decode_responses=True)
 
 print(r.ping())
 print(r.set(name="name", value="Lim"))
